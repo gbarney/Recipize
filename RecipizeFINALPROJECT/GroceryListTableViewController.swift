d6140e51a52ee725f6031c2d579d85c5b975db13
Parin Shah <shahp102@gmail.com>
2015-05-07 22:01:10 -0500

    set up table view for grocery list

---
+++ RecipizeFINALPROJECT/GroceryListTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GroceryListTableViewController.swift
+//  RecipizeFINALPROJECT
+//
+//  Created by pcshah on 5/7/15.
+//  Copyright (c) 2015 Lindsey Finn. All rights reserved.
+//
+
+import UIKit
+
+class GroceryListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var groceryListTableView: UITableView!
+    var foodItems = ["frozen pizza"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return foodItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = groceryListTableView.dequeueReusableCellWithIdentifier("groceryCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = foodItems[indexPath.row]
+        return cell
+    }
+}
