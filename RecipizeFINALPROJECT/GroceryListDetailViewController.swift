8ee90d8271e81a8626fee5d7f4597572a1fbf758
gbarney <gbarney@bsd220.org>
2015-05-11 09:12:48 -0500

    nothing

---
+++ RecipizeFINALPROJECT/GroceryListDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroceryListDetailViewController.swift
+//  RecipizeFINALPROJECT
+//
+//  Created by pcshah on 5/10/15.
+//  Copyright (c) 2015 Lindsey Finn. All rights reserved.
+//
+
+import UIKit
+
+class GroceryListDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var foodListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+    }
+
+}
--- RecipizeFINALPROJECT/GroceryListTableViewController.swift
+++ RecipizeFINALPROJECT/GroceryListTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class GroceryListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var groceryListTableView: UITableView!
-    var foodItems = ["frozen pizza"]
+    var foodItems = ["frozen pizza", "Lettuce", "Tomato", "Cheese"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,4 +28,17 @@ class GroceryListTableViewController: UIViewController, UITableViewDelegate, UIT
         cell.textLabel?.text = foodItems[indexPath.row]
         return cell
     }
+    
+    @IBAction func onEditTappedButton(sender: UIBarButtonItem)
+    {
+        if sender.tag == 0 {
+            groceryListTableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            groceryListTableView.editing = false
+            sender.tag = 0
+        }
+    }
+    
 }
