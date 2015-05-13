4e8b6634f0ecbf2ec4de417332457ef9e0322562
Parin Shah <shahp102@gmail.com>
2015-05-13 18:54:50 -0500

    detail view for individule recipies

---
+++ RecipizeFINALPROJECT/DetailRecipiesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailRecipiesViewController.swift
+//  RecipizeFINALPROJECT
+//
+//  Created by pcshah on 5/13/15.
+//  Copyright (c) 2015 Lindsey Finn. All rights reserved.
+//
+
+import UIKit
+
+class DetailRecipiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
