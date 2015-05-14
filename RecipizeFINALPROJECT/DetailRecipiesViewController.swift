2e4366a5050f7f6457ca9f6078f9f5cc2f3aeed6
Parin Shah <shahp102@gmail.com>
2015-05-13 19:00:08 -0500

    Deleted the detail view for individule recipies because it was to glitchy at the moment

--- RecipizeFINALPROJECT/DetailRecipiesViewController.swift
+++ RecipizeFINALPROJECT/DetailRecipiesViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DetailRecipiesViewController: UIViewController {
 
+    @IBOutlet weak var recipieLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
