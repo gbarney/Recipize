55f0f45a72557785755987baa8738c4ba96945ed
gbarney <gbarney@bsd220.org>
2015-05-15 09:59:11 -0500

    nm

---
+++ RecipizeFINALPROJECT/DetailRecipiesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailRecipiesViewController.swift
+//  RecipizeFINALPROJECT
+//
+//  Created by pcshah on 5/13/15.
+//  Copyright (c) 2015 Lindsey Finn. All rights reserved.
+//
+
+import UIKit
+
+class DetailRecipiesViewController: UIViewController {
+
+    @IBOutlet weak var recipieLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
